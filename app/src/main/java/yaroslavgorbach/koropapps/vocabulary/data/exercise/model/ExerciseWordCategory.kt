1a8eeaac4f67cd50605b8ddc7e5e3b687e18d665
YaroslavGorbach-code <yaroslavgorbach2@gmail.com>
2021-08-02 16:37:22 +0300

    Exercise view refactor

---
+++ app/src/main/java/yaroslavgorbach/koropapps/vocabulary/data/exercise/model/ExerciseWordCategory.kt
@@ -0,0 +1,10 @@
+package yaroslavgorbach.koropapps.vocabulary.data.exercise.model
+
+import yaroslavgorbach.koropapps.vocabulary.R
+
+enum class ExerciseWordCategory(val resId: Int) {
+    LETTERS(R.array.letters),
+    FILLINGS(R.array.fillings),
+    NOUNS(R.array.nouns),
+    CATEGORY(R.array.categories),
+}
