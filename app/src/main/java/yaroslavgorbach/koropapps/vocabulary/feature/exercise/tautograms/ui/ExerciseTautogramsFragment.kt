f1887a8544b67f19ed9b093e81fa4324366d4444
YaroslavGorbach-code <yaroslavgorbach2@gmail.com>
2021-08-02 16:34:35 +0300

    ExerciseTautograms feature refactor

---
+++ app/src/main/java/yaroslavgorbach/koropapps/vocabulary/feature/exercise/tautograms/ui/ExerciseTautogramsFragment.kt
@@ -0,0 +1,38 @@
+package yaroslavgorbach.koropapps.vocabulary.feature.exercise.tautograms.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import yaroslavgorbach.koropapps.vocabulary.R
+import yaroslavgorbach.koropapps.vocabulary.databinding.FragmentExerciseBinding
+import yaroslavgorbach.koropapps.vocabulary.feature.common.uikit.ExerciseView
+import yaroslavgorbach.koropapps.vocabulary.feature.exercise.tautograms.presentation.ExerciseTautogramsViewModel
+
+class ExerciseTautogramsFragment : Fragment(R.layout.fragment_exercise) {
+
+    private val viewModel by viewModels<ExerciseTautogramsViewModel>()
+
+    private lateinit var exerciseView: ExerciseView
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView(view)
+        initObservers()
+    }
+
+    private fun initObservers() {
+        viewModel.word.observe(viewLifecycleOwner, exerciseView::setWord)
+    }
+
+    private fun initView(view: View) {
+        exerciseView = ExerciseView(
+            FragmentExerciseBinding.bind(view),
+            object : ExerciseView.Callback {
+                override fun onNext() {
+                    viewModel.generateWord()
+                }
+            })
+        exerciseView.setDescriptionText(viewModel.descriptionText)
+    }
+}
