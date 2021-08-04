982e703fa97489d4750ba4c6ebc1f95683420b3e
YaroslavGorbach-code <yaroslavgorbach2@gmail.com>
2021-08-04 17:44:14 +0300

    Create liveEvent utils

---
+++ app/src/main/java/yaroslavgorbach/koropapps/vocabulary/util/LiveUtils.kt
@@ -0,0 +1,29 @@
+package yaroslavgorbach.koropapps.vocabulary.util
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import java.util.concurrent.atomic.AtomicBoolean
+
+class Event<T>(val data: T) {
+
+    var consumed: AtomicBoolean = AtomicBoolean(false)
+        private set
+
+    fun consume(consumer: (T) -> Unit) {
+        if (consumed.get().not()) {
+            consumer(data)
+            consumed = AtomicBoolean(true)
+        }
+    }
+}
+
+typealias LiveEvent<T> = LiveData<Event<T>>
+
+typealias MutableLiveEvent<T> = MutableLiveData<Event<T>>
+
+fun <T> MutableLiveEvent<T>.send(data: T) = postValue(Event(data))
+
+fun <T> LiveEvent<T>.consume(lifecycleOwner: LifecycleOwner, consumer: (T) -> Unit) {
+    observe(lifecycleOwner, { it.consume(consumer) })
+}
