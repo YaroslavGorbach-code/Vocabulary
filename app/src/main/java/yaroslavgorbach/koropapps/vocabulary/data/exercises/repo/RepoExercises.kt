87ea7d5e4397d073903714ae01f099f15f42a047
YaroslavGorbach-code <yaroslavgorbach2@gmail.com>
2021-08-09 18:17:58 +0300

    Repo exercises refactor

--- app/src/main/java/yaroslavgorbach/koropapps/vocabulary/data/exercises/repo/RepoExercises.kt
+++ app/src/main/java/yaroslavgorbach/koropapps/vocabulary/data/exercises/repo/RepoExercises.kt
@@ -1,10 +1,11 @@
 package yaroslavgorbach.koropapps.vocabulary.data.exercises.repo
 
-import kotlinx.coroutines.flow.Flow
+import io.reactivex.rxjava3.core.Single
 import yaroslavgorbach.koropapps.vocabulary.data.exercises.local.model.Exercise
 import yaroslavgorbach.koropapps.vocabulary.data.exercises.local.model.ExerciseName
+import yaroslavgorbach.koropapps.vocabulary.feature.exerciseslist.model.ExerciseUi
 
 interface RepoExercises {
-    suspend fun getExercises(): Flow<List<Exercise>>
-    fun getExercise(exerciseName: ExerciseName): Exercise
+    fun get(): Single<List<Exercise>>
+    fun get(exerciseName: ExerciseName): Single<Exercise>
 }
--- app/src/main/java/yaroslavgorbach/koropapps/vocabulary/data/exercises/repo/RepoExercisesImp.kt
+++ app/src/main/java/yaroslavgorbach/koropapps/vocabulary/data/exercises/repo/RepoExercisesImp.kt
@@ -1,37 +1,39 @@
 package yaroslavgorbach.koropapps.vocabulary.data.exercises.repo
 
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import yaroslavgorbach.koropapps.vocabulary.R
+import io.reactivex.rxjava3.core.Single
 import yaroslavgorbach.koropapps.vocabulary.data.exercises.local.model.Exercise
 import yaroslavgorbach.koropapps.vocabulary.data.exercises.local.model.ExerciseName
 
 class RepoExercisesImp : RepoExercises {
-    private val exercises: MutableStateFlow<List<Exercise>> = MutableStateFlow(emptyList())
-
-    init {
-        exercises.value = listOf(
-            Exercise(ExerciseName.ALPHABET_ADJECTIVES, R.drawable.ic_alphabet_a),
-            Exercise(ExerciseName.ALPHABET_NOUN, R.drawable.ic_alphabet_n),
-            Exercise(ExerciseName.ALPHABET_VERBS, R.drawable.ic_alphabet_v),
-            Exercise(ExerciseName.TAUTOGRAMS, R.drawable.ic_promotional_n),
-            Exercise(ExerciseName.NARRATOR_NOUN, R.drawable.ic_narrator_n),
-            Exercise(ExerciseName.NARRATOR_ADJECTIVES, R.drawable.ic_narrator_a),
-            Exercise(ExerciseName.NARRATOR_VERBS, R.drawable.ic_narrator_v),
-            Exercise(ExerciseName.ANTONYMS_AND_SYNONYMS, R.drawable.ic_arrows),
-            Exercise(ExerciseName.ASSOCIATIONS, R.drawable.ic_assotiations),
-            Exercise(ExerciseName.REMEMBER_ALL, R.drawable.ic_test),
-            Exercise(ExerciseName.GAME_I_KNOW_5_NAMES, R.drawable.ic_game),
-            Exercise(ExerciseName.TEN, R.drawable.ic_ten),
-            Exercise(ExerciseName.THREE_LITER_JAR, R.drawable.ic_jar),
+    private val exercises: Single<List<Exercise>> = Single.just(
+        listOf(
+            Exercise(ExerciseName.ALPHABET_ADJECTIVES),
+            Exercise(ExerciseName.ALPHABET_NOUN),
+            Exercise(ExerciseName.ALPHABET_VERBS),
+            Exercise(ExerciseName.TAUTOGRAMS),
+            Exercise(ExerciseName.NARRATOR_NOUN),
+            Exercise(ExerciseName.NARRATOR_ADJECTIVES),
+            Exercise(ExerciseName.NARRATOR_VERBS),
+            Exercise(ExerciseName.ANTONYMS_AND_SYNONYMS),
+            Exercise(ExerciseName.ASSOCIATIONS),
+            Exercise(ExerciseName.REMEMBER_ALL),
+            Exercise(ExerciseName.GAME_I_KNOW_5_NAMES),
+            Exercise(ExerciseName.TEN),
+            Exercise(ExerciseName.THREE_LITER_JAR),
         )
-    }
+    )
 
-    override suspend fun getExercises(): Flow<List<Exercise>> {
+    override fun get(): Single<List<Exercise>> {
         return exercises
+
     }
 
-    override fun getExercise(exerciseName: ExerciseName): Exercise {
-       return exercises.value.find { exercise -> exercise.name == exerciseName }!!
+    override fun get(exerciseName: ExerciseName): Single<Exercise> {
+        return exercises
+            .map { list ->
+                list.first { exercise ->
+                    exercise.name == exerciseName
+                }
+            }
     }
 }
