fbf2f8bc8b59498a7f5b7686fb5a85e074f98332
YaroslavGorbach-code <yaroslavgorbach2@gmail.com>
2021-08-10 18:47:54 +0300

    Update exercise list feature ViewModel(add training)

--- app/src/main/java/yaroslavgorbach/koropapps/vocabulary/feature/exerciseslist/presentation/ExercisesListViewModel.kt
+++ app/src/main/java/yaroslavgorbach/koropapps/vocabulary/feature/exerciseslist/presentation/ExercisesListViewModel.kt
@@ -5,10 +5,14 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.functions.Consumer
-import yaroslavgorbach.koropapps.vocabulary.business.exercises.GetExercisesUseCase
+import yaroslavgorbach.koropapps.vocabulary.business.exercises.GetExercisesIntearactor
+import yaroslavgorbach.koropapps.vocabulary.business.training.ObserveTrainingsInteractor
 import yaroslavgorbach.koropapps.vocabulary.data.exercises.repo.RepoExercises
 import yaroslavgorbach.koropapps.vocabulary.data.exercises.repo.RepoExercisesImp
+import yaroslavgorbach.koropapps.vocabulary.data.training.repo.RepoTraining
+import yaroslavgorbach.koropapps.vocabulary.data.training.repo.RepoTrainingImp
 import yaroslavgorbach.koropapps.vocabulary.feature.exerciseslist.model.ExerciseUi
+import yaroslavgorbach.koropapps.vocabulary.feature.exerciseslist.model.TrainingUi
 
 class ExercisesListViewModel : ViewModel() {
 
@@ -17,22 +21,43 @@ class ExercisesListViewModel : ViewModel() {
     private val exercisesRepo: RepoExercises
         get() = RepoExercisesImp()
 
-    private val getExercisesUseCase: GetExercisesUseCase
-        get() = GetExercisesUseCase(exercisesRepo)
+    private val repoTraining: RepoTraining
+        get() = RepoTrainingImp()
+
+    private val observeTrainingsInteractor: ObserveTrainingsInteractor
+        get() = ObserveTrainingsInteractor(repoTraining)
+
+    private val getExercisesInteractor: GetExercisesIntearactor
+        get() = GetExercisesIntearactor(exercisesRepo)
 
     private val _exercises: MutableLiveData<List<ExerciseUi>> = MutableLiveData()
 
     val exercises: LiveData<List<ExerciseUi>>
         get() = _exercises
 
+    private val _training: MutableLiveData<TrainingUi> = MutableLiveData()
+
+    val training: LiveData<TrainingUi>
+        get() = _training
+
     init {
         getExercises()
+        getTraining()
+    }
+
+    private fun getTraining() {
+        observeTrainingsInteractor()
+            .map { it.take(5) }
+            .map(::TrainingUi)
+            .subscribe(_training::postValue)
+            .let(disposables::add)
+
     }
 
     private fun getExercises() {
-        getExercisesUseCase()
+        getExercisesInteractor()
             .map { list -> list.map(::ExerciseUi) }
-            .subscribe(Consumer(_exercises::setValue))
+            .subscribe(Consumer(_exercises::postValue))
             .let(disposables::add)
     }
 
--- app/src/main/java/yaroslavgorbach/koropapps/vocabulary/feature/exerciseslist/ui/ExercisesListView.kt
+++ app/src/main/java/yaroslavgorbach/koropapps/vocabulary/feature/exerciseslist/ui/ExercisesListView.kt
@@ -1,10 +1,12 @@
 package yaroslavgorbach.koropapps.vocabulary.feature.exerciseslist.ui
 
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import yaroslavgorbach.koropapps.vocabulary.R
 import yaroslavgorbach.koropapps.vocabulary.databinding.FragmentExercisesListBinding
 import yaroslavgorbach.koropapps.vocabulary.feature.common.uikit.LineDecorator
 import yaroslavgorbach.koropapps.vocabulary.feature.exerciseslist.model.ExerciseUi
+import yaroslavgorbach.koropapps.vocabulary.feature.exerciseslist.model.TrainingUi
 import yaroslavgorbach.koropapps.vocabulary.feature.exerciseslist.ui.recycler.ExercisesListAdapter
 
 class ExercisesListView(
@@ -38,5 +40,24 @@ class ExercisesListView(
 
     fun setExercises(list: List<ExerciseUi>) {
         listAdapter.setData(list)
+    }
+
+    fun setTraining(trainingUi: TrainingUi) {
+        with(binding.training.days) {
+            day1.setProgress(trainingUi.first.progress)
+            day1.setText(trainingUi.first.id.toString())
+
+            day2.setProgress(trainingUi.second.progress)
+            day2.setText(trainingUi.second.id.toString())
+
+            day3.setProgress(trainingUi.third.progress)
+            day3.setText(trainingUi.third.id.toString())
+
+            day4.setProgress(trainingUi.fourth.progress)
+            day4.setText(trainingUi.fourth.id.toString())
+
+            day5.setProgress(trainingUi.fifth.progress)
+            day5.setText(trainingUi.fifth.id.toString())
+        }
     }
 }
