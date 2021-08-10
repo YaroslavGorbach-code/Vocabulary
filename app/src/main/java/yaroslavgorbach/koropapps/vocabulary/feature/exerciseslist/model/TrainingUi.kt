ac0f614b9a26d4716b026c5c7c1974c18841f51f
YaroslavGorbach-code <yaroslavgorbach2@gmail.com>
2021-08-10 18:45:03 +0300

    Create training models (ui and data)

---
+++ app/src/main/java/yaroslavgorbach/koropapps/vocabulary/feature/exerciseslist/model/TrainingUi.kt
@@ -0,0 +1,20 @@
+package yaroslavgorbach.koropapps.vocabulary.feature.exerciseslist.model
+
+import yaroslavgorbach.koropapps.vocabulary.data.training.local.model.Training
+
+data class TrainingUi(val trainings: List<Training>) {
+    val first: Training
+        get() = trainings[0]
+
+    val second: Training
+        get() = trainings[1]
+
+    val third: Training
+        get() = trainings[2]
+
+    val fourth: Training
+        get() = trainings[3]
+
+    val fifth: Training
+        get() = trainings[4]
+}
