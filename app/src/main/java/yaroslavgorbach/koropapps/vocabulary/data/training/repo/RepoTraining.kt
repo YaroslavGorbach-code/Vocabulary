5272d78372ed17ac6dc3514a95e9010d61694368
YaroslavGorbach-code <yaroslavgorbach2@gmail.com>
2021-08-10 18:45:37 +0300

    Implement training repo with test data

--- app/src/main/java/yaroslavgorbach/koropapps/vocabulary/data/training/repo/RepoTraining.kt
+++ app/src/main/java/yaroslavgorbach/koropapps/vocabulary/data/training/repo/RepoTraining.kt
@@ -1,4 +1,11 @@
 package yaroslavgorbach.koropapps.vocabulary.data.training.repo
 
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Observable
+import yaroslavgorbach.koropapps.vocabulary.data.training.local.model.Training
+
 interface RepoTraining {
+    fun observe(): Observable<List<Training>>
+    fun insert(training: Training): Completable
+    fun insert(trainings: List<Training>): Completable
 }
---
+++ app/src/main/java/yaroslavgorbach/koropapps/vocabulary/data/training/repo/RepoTrainingImp.kt
@@ -0,0 +1,58 @@
+package yaroslavgorbach.koropapps.vocabulary.data.training.repo
+
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Observable
+import yaroslavgorbach.koropapps.vocabulary.data.exercises.local.model.ExerciseName
+import yaroslavgorbach.koropapps.vocabulary.data.training.local.model.ExerciseTraining
+import yaroslavgorbach.koropapps.vocabulary.data.training.local.model.Training
+import java.util.*
+
+class RepoTrainingImp : RepoTraining {
+
+    private val testTrainings: MutableList<Training> = createTestTrainings().toMutableList()
+
+    override fun observe(): Observable<List<Training>> {
+        return Observable.just(testTrainings)
+    }
+
+    override fun insert(training: Training): Completable {
+        insert(listOf(training))
+        // TODO: 8/10/2021 fix it then with room
+        return Completable.create { it.onComplete() }
+    }
+
+    override fun insert(trainings: List<Training>): Completable {
+        testTrainings.addAll(trainings)
+        // TODO: 8/10/2021 fix it then with room
+        return Completable.create { it.onComplete() }
+    }
+
+    private fun createTestTrainings(): List<Training> {
+        return listOf(
+            Training(1, Date(), createTestExercises()),
+            Training(2, Date(), createTestExercises()),
+            Training(3, Date(), createTestExercises()),
+            Training(4, Date(), createTestExercises()),
+            Training(5, Date(), createTestExercises()),
+            Training(6, Date(), createTestExercises()),
+            Training(7, Date(), createTestExercises())
+        )
+    }
+
+    private fun createTestExercises(): List<ExerciseTraining> {
+        return listOf(
+            ExerciseTraining(
+                1,
+                ExerciseName.TEN,
+                100,
+                10,
+            ),
+            ExerciseTraining(
+                2,
+                ExerciseName.ANTONYMS_AND_SYNONYMS,
+                100,
+                10,
+            )
+        )
+    }
+}
