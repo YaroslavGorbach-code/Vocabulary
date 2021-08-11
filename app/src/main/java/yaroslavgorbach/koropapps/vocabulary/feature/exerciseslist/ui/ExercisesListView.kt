66561804fe39383bed72e027812bc55ad19f0f8f
YaroslavGorbach-code <yaroslavgorbach2@gmail.com>
2021-08-11 15:08:15 +0300

    Implement show current day on trainings

--- app/src/main/java/yaroslavgorbach/koropapps/vocabulary/feature/exerciseslist/ui/ExercisesListView.kt
+++ app/src/main/java/yaroslavgorbach/koropapps/vocabulary/feature/exerciseslist/ui/ExercisesListView.kt
@@ -1,6 +1,5 @@
 package yaroslavgorbach.koropapps.vocabulary.feature.exerciseslist.ui
 
-import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import yaroslavgorbach.koropapps.vocabulary.R
 import yaroslavgorbach.koropapps.vocabulary.databinding.FragmentExercisesListBinding
@@ -8,6 +7,7 @@ import yaroslavgorbach.koropapps.vocabulary.feature.common.uikit.LineDecorator
 import yaroslavgorbach.koropapps.vocabulary.feature.exerciseslist.model.ExerciseUi
 import yaroslavgorbach.koropapps.vocabulary.feature.exerciseslist.model.TrainingUi
 import yaroslavgorbach.koropapps.vocabulary.feature.exerciseslist.ui.recycler.ExercisesListAdapter
+import yaroslavgorbach.koropapps.vocabulary.utils.day
 
 class ExercisesListView(
     private val binding: FragmentExercisesListBinding,
@@ -45,19 +45,19 @@ class ExercisesListView(
     fun setTraining(trainingUi: TrainingUi) {
         with(binding.training.days) {
             day1.setProgress(trainingUi.first.progress)
-            day1.setText(trainingUi.first.id.toString())
+            day1.setText(trainingUi.first.date.day())
 
             day2.setProgress(trainingUi.second.progress)
-            day2.setText(trainingUi.second.id.toString())
+            day2.setText(trainingUi.first.date.day())
 
             day3.setProgress(trainingUi.third.progress)
-            day3.setText(trainingUi.third.id.toString())
+            day3.setText(trainingUi.first.date.day())
 
             day4.setProgress(trainingUi.fourth.progress)
-            day4.setText(trainingUi.fourth.id.toString())
+            day4.setText(trainingUi.first.date.day())
 
             day5.setProgress(trainingUi.fifth.progress)
-            day5.setText(trainingUi.fifth.id.toString())
+            day5.setText(trainingUi.first.date.day())
         }
     }
 }
