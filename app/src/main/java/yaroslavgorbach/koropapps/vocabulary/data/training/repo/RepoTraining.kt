ed03c25573b43c98651642d58ad7d6099fa6f1b2
Бабенко Владимир <>
2021-08-12 21:42:47 +0300

    Refactor training repo to work with room

--- app/src/main/java/yaroslavgorbach/koropapps/vocabulary/data/training/repo/RepoTraining.kt
+++ app/src/main/java/yaroslavgorbach/koropapps/vocabulary/data/training/repo/RepoTraining.kt
@@ -2,10 +2,11 @@ package yaroslavgorbach.koropapps.vocabulary.data.training.repo
 
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Observable
-import yaroslavgorbach.koropapps.vocabulary.data.training.local.model.Training
+import yaroslavgorbach.koropapps.vocabulary.data.training.local.model.TrainingEntity
+import yaroslavgorbach.koropapps.vocabulary.data.training.local.model.TrainingWithExercises
 
 interface RepoTraining {
-    fun observe(): Observable<List<Training>>
-    fun insert(training: Training): Completable
-    fun insert(trainings: List<Training>): Completable
+    fun observe(): Observable<List<TrainingWithExercises>>
+
+    fun insert(trainingEntity: TrainingEntity): Completable
 }
--- app/src/main/java/yaroslavgorbach/koropapps/vocabulary/data/training/repo/RepoTrainingImp.kt
+++ app/src/main/java/yaroslavgorbach/koropapps/vocabulary/data/training/repo/RepoTrainingImp.kt
@@ -2,57 +2,18 @@ package yaroslavgorbach.koropapps.vocabulary.data.training.repo
 
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Observable
-import yaroslavgorbach.koropapps.vocabulary.data.exercises.local.model.ExerciseName
-import yaroslavgorbach.koropapps.vocabulary.data.training.local.model.ExerciseTraining
-import yaroslavgorbach.koropapps.vocabulary.data.training.local.model.Training
-import java.util.*
+import yaroslavgorbach.koropapps.vocabulary.data.training.local.dao.TrainingDao
+import yaroslavgorbach.koropapps.vocabulary.data.training.local.model.TrainingEntity
+import yaroslavgorbach.koropapps.vocabulary.data.training.local.model.TrainingWithExercises
 
-class RepoTrainingImp : RepoTraining {
+class RepoTrainingImp(private val localDataSource: TrainingDao) : RepoTraining {
 
-    private val testTrainings: MutableList<Training> = createTestTrainings().toMutableList()
-
-    override fun observe(): Observable<List<Training>> {
-        return Observable.just(testTrainings)
-            .map { list ->
-                while (list.size < 5) {
-                    list.add(Training(0, null, emptyList()))
-                }
-                list.reversed()
-            }
+    override fun observe(): Observable<List<TrainingWithExercises>> {
+        return localDataSource.observe()
     }
 
-    override fun insert(training: Training): Completable {
-        insert(listOf(training))
-        // TODO: 8/10/2021 fix it then with room
-        return Completable.create { it.onComplete() }
+    override fun insert(trainingEntity: TrainingEntity): Completable {
+        return localDataSource.insert(trainingEntity)
     }
 
-    override fun insert(trainings: List<Training>): Completable {
-        testTrainings.addAll(trainings)
-        // TODO: 8/10/2021 fix it then with room
-        return Completable.create { it.onComplete() }
-    }
-
-    private fun createTestTrainings(): List<Training> {
-        return listOf(
-            Training(1, Date(), createTestExercises()),
-        )
-    }
-
-    private fun createTestExercises(): List<ExerciseTraining> {
-        return listOf(
-            ExerciseTraining(
-                1,
-                ExerciseName.TEN,
-                100,
-                10,
-            ),
-            ExerciseTraining(
-                2,
-                ExerciseName.ANTONYMS_AND_SYNONYMS,
-                100,
-                10,
-            )
-        )
-    }
 }
