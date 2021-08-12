76ec6d588f78539daf528e569f23bd01334cffb3
Бабенко Владимир <>
2021-08-12 21:46:58 +0300

    Refactor ExercisesListViewModel to work with local data source

--- app/src/main/java/yaroslavgorbach/koropapps/vocabulary/feature/exerciseslist/presentation/ExercisesListViewModel.kt
+++ app/src/main/java/yaroslavgorbach/koropapps/vocabulary/feature/exerciseslist/presentation/ExercisesListViewModel.kt
@@ -1,34 +1,40 @@
 package yaroslavgorbach.koropapps.vocabulary.feature.exerciseslist.presentation
 
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import io.reactivex.rxjava3.disposables.CompositeDisposable
-import io.reactivex.rxjava3.functions.Consumer
-import yaroslavgorbach.koropapps.vocabulary.business.exercises.GetExercisesIntearactor
+import yaroslavgorbach.koropapps.vocabulary.business.exercises.GetExercisesInteract
 import yaroslavgorbach.koropapps.vocabulary.business.training.ObserveTrainingsInteractor
 import yaroslavgorbach.koropapps.vocabulary.data.exercises.repo.RepoExercises
 import yaroslavgorbach.koropapps.vocabulary.data.exercises.repo.RepoExercisesImp
+import yaroslavgorbach.koropapps.vocabulary.data.training.local.TrainingDatabase
 import yaroslavgorbach.koropapps.vocabulary.data.training.repo.RepoTraining
 import yaroslavgorbach.koropapps.vocabulary.data.training.repo.RepoTrainingImp
 import yaroslavgorbach.koropapps.vocabulary.feature.exerciseslist.model.ExerciseUi
 import yaroslavgorbach.koropapps.vocabulary.feature.exerciseslist.model.TrainingUi
+import yaroslavgorbach.koropapps.vocabulary.feature.exerciseslist.presentation.factory.TrainingFactory
 
-class ExercisesListViewModel : ViewModel() {
+class ExercisesListViewModel(application: Application) : AndroidViewModel(application) {
 
     private val disposables: CompositeDisposable = CompositeDisposable()
+
+    private val trainingDatabase: TrainingDatabase
+        get() = TrainingDatabase.getInstance(getApplication())
 
     private val exercisesRepo: RepoExercises
         get() = RepoExercisesImp()
 
     private val repoTraining: RepoTraining
-        get() = RepoTrainingImp()
+        get() = RepoTrainingImp(trainingDatabase.trainingDao)
 
     private val observeTrainingsInteractor: ObserveTrainingsInteractor
         get() = ObserveTrainingsInteractor(repoTraining)
 
-    private val getExercisesInteractor: GetExercisesIntearactor
-        get() = GetExercisesIntearactor(exercisesRepo)
+    private val getExercisesInteractor: GetExercisesInteract
+        get() = GetExercisesInteract(exercisesRepo)
 
     private val _exercises: MutableLiveData<List<ExerciseUi>> = MutableLiveData()
 
@@ -47,7 +53,11 @@ class ExercisesListViewModel : ViewModel() {
 
     private fun getTraining() {
         observeTrainingsInteractor()
-            .map { it.take(5) }
+            .doOnNext { list ->
+                while (list.size < 5) {
+                    list.toMutableList().add(TrainingFactory().create())
+                }
+            }
             .map(::TrainingUi)
             .subscribe(_training::postValue)
             .let(disposables::add)
@@ -57,7 +67,7 @@ class ExercisesListViewModel : ViewModel() {
     private fun getExercises() {
         getExercisesInteractor()
             .map { list -> list.map(::ExerciseUi) }
-            .subscribe(Consumer(_exercises::postValue))
+            .subscribe(_exercises::postValue)
             .let(disposables::add)
     }
 
