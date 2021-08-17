a32047cfe9b9c6758d14928f961d7a6660e6bae9
Yaroslav <>
2021-08-17 18:02:35 +0300

    Create interactor to observe current training with exercises

--- app/src/main/java/yaroslavgorbach/koropapps/vocabulary/business/training/ObserveCurrentTrainingWithExercisesInteractor.kt
+++ app/src/main/java/yaroslavgorbach/koropapps/vocabulary/business/training/ObserveCurrentTrainingWithExercisesInteractor.kt
@@ -1,13 +1,23 @@
 package yaroslavgorbach.koropapps.vocabulary.business.training
 
+import android.util.Log
 import io.reactivex.rxjava3.core.Observable
+import yaroslavgorbach.koropapps.vocabulary.business.training.factory.TrainingExercisesFactory
 import yaroslavgorbach.koropapps.vocabulary.data.training.local.model.TrainingWithExercisesEntity
 
 class ObserveCurrentTrainingWithExercisesInteractor(
-    private val observeLastFifeTrainingsInteractor: ObserveLastFifeTrainingsInteractor
+    private val observeLastFifeTrainingsInteractor: ObserveLastFifeTrainingsInteractor,
+    private val insertTrainingExercisesInteractor: InsertTrainingExercisesInteractor
 ) {
     operator fun invoke(): Observable<TrainingWithExercisesEntity> {
         return observeLastFifeTrainingsInteractor()
             .map { list -> list.last() }
+            .doOnNext { training ->
+                if (training.exercises.isEmpty()) {
+                    insertTrainingExercisesInteractor(
+                        TrainingExercisesFactory().create(training.training)
+                    ).subscribe()
+                }
+            }
     }
 }
--- app/src/main/java/yaroslavgorbach/koropapps/vocabulary/feature/training/presentation/TrainingViewModel.kt
+++ app/src/main/java/yaroslavgorbach/koropapps/vocabulary/feature/training/presentation/TrainingViewModel.kt
@@ -1,14 +1,10 @@
 package yaroslavgorbach.koropapps.vocabulary.feature.training.presentation
-
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import io.reactivex.rxjava3.disposables.CompositeDisposable
-import yaroslavgorbach.koropapps.vocabulary.business.training.InsertTrainingInteractor
-import yaroslavgorbach.koropapps.vocabulary.business.training.ObserveCurrentTrainingWithExercisesInteractor
-import yaroslavgorbach.koropapps.vocabulary.business.training.ObserveLastFifeTrainingsInteractor
-import yaroslavgorbach.koropapps.vocabulary.business.training.ObserveTrainingsInteractor
+import yaroslavgorbach.koropapps.vocabulary.business.training.*
 import yaroslavgorbach.koropapps.vocabulary.data.training.local.TrainingDatabase
 import yaroslavgorbach.koropapps.vocabulary.data.training.repo.RepoTraining
 import yaroslavgorbach.koropapps.vocabulary.data.training.repo.RepoTrainingImp
@@ -30,6 +26,9 @@ class TrainingViewModel(application: Application) : AndroidViewModel(application
     private val insertTrainingInteractor: InsertTrainingInteractor
         get() = InsertTrainingInteractor(repoTraining)
 
+    private val insertTrainingExercisesInteractor: InsertTrainingExercisesInteractor
+        get() = InsertTrainingExercisesInteractor(repoTraining)
+
     private val observeLastFifeTrainingsInteractor: ObserveLastFifeTrainingsInteractor
         get() = ObserveLastFifeTrainingsInteractor(
             observeTrainingsInteractor,
@@ -37,7 +36,10 @@ class TrainingViewModel(application: Application) : AndroidViewModel(application
         )
 
     private val observeCurrentTrainingWithExercisesInteractor: ObserveCurrentTrainingWithExercisesInteractor
-        get() = ObserveCurrentTrainingWithExercisesInteractor(observeLastFifeTrainingsInteractor)
+        get() = ObserveCurrentTrainingWithExercisesInteractor(
+            observeLastFifeTrainingsInteractor,
+            insertTrainingExercisesInteractor
+        )
 
     private val _trainingWithExercises: MutableLiveData<TrainingWithExercisesUi> = MutableLiveData()
 
