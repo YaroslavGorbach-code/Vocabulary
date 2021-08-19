06a1aa27f7f8d29bc1d2fd1deaa1b29ec2a3b569
Yaroslav <yaroslavgorbach2@gmail.com>
2021-08-19 16:15:47 +0300

    Create data module exercises

---
+++ app/src/main/java/yaroslavgorbach/koropapps/vocabulary/di/data/exercises/DataModuleExercises.kt
@@ -0,0 +1,17 @@
+package yaroslavgorbach.koropapps.vocabulary.di.data.exercises
+
+import dagger.Module
+import dagger.Provides
+import yaroslavgorbach.koropapps.vocabulary.data.exercises.repo.RepoExercises
+import yaroslavgorbach.koropapps.vocabulary.data.exercises.repo.RepoExercisesImp
+import javax.inject.Singleton
+
+@Module
+class DataModuleExercises {
+    @Singleton
+    @Provides
+    fun provideRepoExercises(): RepoExercises {
+        return RepoExercisesImp()
+    }
+
+}
