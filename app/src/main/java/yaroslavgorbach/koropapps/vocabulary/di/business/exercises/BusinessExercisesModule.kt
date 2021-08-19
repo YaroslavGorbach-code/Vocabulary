523c79e3f30e914c88af655e5fc2b419c65cbdad
Yaroslav <yaroslavgorbach2@gmail.com>
2021-08-19 16:15:10 +0300

    Create business exercise module

---
+++ app/src/main/java/yaroslavgorbach/koropapps/vocabulary/di/business/exercises/BusinessExercisesModule.kt
@@ -0,0 +1,22 @@
+package yaroslavgorbach.koropapps.vocabulary.di.business.exercises
+
+import dagger.Module
+import dagger.Provides
+import yaroslavgorbach.koropapps.vocabulary.business.exercises.GetExercisesInteractor
+import yaroslavgorbach.koropapps.vocabulary.data.exercises.repo.RepoExercises
+import yaroslavgorbach.koropapps.vocabulary.di.business.training.BusinessTrainingModule
+import yaroslavgorbach.koropapps.vocabulary.di.data.exercises.DataModuleExercises
+import yaroslavgorbach.koropapps.vocabulary.feature.exerciseslist.di.ExercisesListComponent
+import javax.inject.Singleton
+
+@Module(
+    includes = [DataModuleExercises::class, BusinessTrainingModule::class],
+    subcomponents = [ExercisesListComponent::class]
+)
+class BusinessExercisesModule {
+    @Singleton
+    @Provides
+    fun provideGetExercisesInteractor(repoExercises: RepoExercises): GetExercisesInteractor {
+        return GetExercisesInteractor(repoExercises)
+    }
+}
