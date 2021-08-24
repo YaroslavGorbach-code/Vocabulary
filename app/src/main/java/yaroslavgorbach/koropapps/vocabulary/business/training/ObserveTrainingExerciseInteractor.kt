2c07822e449415602e5cd799c5a2cdac9795bc7f
Yaroslav <yaroslavgorbach2@gmail.com>
2021-08-24 14:05:31 +0300

    Minor interactors improvements

--- app/src/main/java/yaroslavgorbach/koropapps/vocabulary/business/training/ObserveTrainingExerciseInteractor.kt
+++ app/src/main/java/yaroslavgorbach/koropapps/vocabulary/business/training/ObserveTrainingExerciseInteractor.kt
@@ -1,11 +1,12 @@
 package yaroslavgorbach.koropapps.vocabulary.business.training
 
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.schedulers.Schedulers
 import yaroslavgorbach.koropapps.vocabulary.data.training.local.model.TrainingExerciseEntity
 import yaroslavgorbach.koropapps.vocabulary.data.training.repo.RepoTraining
 
 class ObserveTrainingExerciseInteractor(private val repoTraining: RepoTraining) {
     operator fun invoke(exerciseId: Long): Observable<TrainingExerciseEntity> {
-        return repoTraining.observeExercise(exerciseId)
+        return repoTraining.observeExercise(exerciseId).subscribeOn(Schedulers.io())
     }
 }
