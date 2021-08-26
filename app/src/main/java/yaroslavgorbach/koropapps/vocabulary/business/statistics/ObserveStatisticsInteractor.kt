c405975a1e14cebd3c708065cd81d3b461002bd8
Yaroslav <yaroslavgorbach2@gmail.com>
2021-08-26 17:57:24 +0300

    Create ObserveStatisticsInteractor.kt

---
+++ app/src/main/java/yaroslavgorbach/koropapps/vocabulary/business/statistics/ObserveStatisticsInteractor.kt
@@ -0,0 +1,11 @@
+package yaroslavgorbach.koropapps.vocabulary.business.statistics
+
+import io.reactivex.rxjava3.core.Observable
+import yaroslavgorbach.koropapps.vocabulary.data.statistics.local.model.StatisticsEntity
+import yaroslavgorbach.koropapps.vocabulary.data.statistics.repo.RepoStatistics
+
+class ObserveStatisticsInteractor(private val repoStatistics: RepoStatistics) {
+    operator fun invoke(exerciseNameRes: Int): Observable<List<StatisticsEntity>> {
+        return repoStatistics.observe(exerciseNameRes)
+    }
+}
