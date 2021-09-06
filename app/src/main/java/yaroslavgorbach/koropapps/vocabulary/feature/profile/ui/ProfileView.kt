a487fd0ab7be1110b5b1e2a979cb61cd90ec460f
Yaroslav <yaroslavgorbach2@gmail.com>
2021-09-06 13:45:00 +0300

    Improve profile layout

--- app/src/main/java/yaroslavgorbach/koropapps/vocabulary/feature/profile/ui/ProfileView.kt
+++ app/src/main/java/yaroslavgorbach/koropapps/vocabulary/feature/profile/ui/ProfileView.kt
@@ -2,18 +2,47 @@ package yaroslavgorbach.koropapps.vocabulary.feature.profile.ui
 
 import android.view.View
 import im.dacer.androidcharts.LineView
+import yaroslavgorbach.koropapps.vocabulary.R
 import yaroslavgorbach.koropapps.vocabulary.databinding.FragmentProfileBinding
 import yaroslavgorbach.koropapps.vocabulary.feature.profile.model.ChartDayUi
+import yaroslavgorbach.koropapps.vocabulary.utils.getDrawable
 
 class ProfileView(private val binding: FragmentProfileBinding, private val callback: Callback) {
 
     interface Callback {
         fun onNextChart()
         fun onPreviousChart()
+        fun onSettings()
+        fun onRemoveAds()
+        fun onShare()
+        fun onRate()
     }
 
     init {
+        initViews()
         initActions()
+    }
+
+    private fun initViews() {
+        with(binding.settings) {
+            icon.setImageDrawable(getDrawable(R.drawable.ic_settings))
+            text.setText(R.string.settings)
+        }
+
+        with(binding.removeAds) {
+            icon.setImageDrawable(getDrawable(R.drawable.ic_no_ads))
+            text.setText(R.string.remove_ads)
+        }
+
+        with(binding.rate) {
+            icon.setImageDrawable(getDrawable(R.drawable.ic_rate))
+            text.setText(R.string.rate_app)
+        }
+
+        with(binding.share) {
+            icon.setImageDrawable(getDrawable(R.drawable.ic_share))
+            text.setText(R.string.share_app)
+        }
     }
 
     private fun initActions() {
@@ -23,6 +52,22 @@ class ProfileView(private val binding: FragmentProfileBinding, private val callb
 
         binding.chart.nextData.setOnClickListener {
             callback.onNextChart()
+        }
+
+        binding.rate.item.setOnClickListener {
+            callback.onRate()
+        }
+
+        binding.removeAds.item.setOnClickListener {
+            callback.onRemoveAds()
+        }
+
+        binding.share.item.setOnClickListener {
+            callback.onShare()
+        }
+
+        binding.settings.item.setOnClickListener {
+            callback.onSettings()
         }
     }
 
