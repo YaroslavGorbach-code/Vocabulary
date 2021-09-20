2c576adb71f1d9335cb5c9826682a333cc800089
Yaroslav <yaroslavgorbach2@gmail.com>
2021-09-20 16:35:22 +0300

    Refactor app style resources

--- app/src/main/java/yaroslavgorbach/koropapps/vocabulary/data/settings/local/model/Theme.kt
+++ app/src/main/java/yaroslavgorbach/koropapps/vocabulary/data/settings/local/model/Theme.kt
@@ -1,10 +1,46 @@
 package yaroslavgorbach.koropapps.vocabulary.data.settings.local.model
 
+import android.graphics.Color
+import yaroslavgorbach.koropapps.vocabulary.R
+
 // TODO: 9/16/2021 add themes
 sealed class Theme {
     abstract val res: Int
 
-    class TestFirst(override val res: Int = 0) : Theme()
+    abstract val colorPrimary: Int
 
-    class TestSecond(override val res: Int = 0) : Theme()
+    class Base(
+        override val res: Int = R.style.BaseAppTheme,
+        override val colorPrimary: Int = Color.parseColor("#4db6ac")
+    ) : Theme()
+
+    class Blue(
+        override val res: Int = R.style.BaseAppTheme_Blue,
+        override val colorPrimary: Int = Color.parseColor("#64b5f6")
+    ) : Theme()
+
+    class Indigo(
+        override val res: Int = R.style.BaseAppTheme_Indigo,
+        override val colorPrimary: Int = Color.parseColor("#5c6bc0")
+    ) : Theme()
+
+    class Purple(
+        override val res: Int = R.style.BaseAppTheme_Purple,
+        override val colorPrimary: Int = Color.parseColor("#7e57c2")
+    ) : Theme()
+
+    class DeepPurple(
+        override val res: Int = R.style.BaseAppTheme_DeepPurple,
+        override val colorPrimary: Int = Color.parseColor("#ba68c8")
+    ) : Theme()
+
+    class Pink(
+        override val res: Int = R.style.BaseAppTheme_Pink,
+        override val colorPrimary: Int = Color.parseColor("#f06292")
+    ) : Theme()
+
+    class Red(
+        override val res: Int = R.style.BaseAppTheme_Red,
+        override val colorPrimary: Int = Color.parseColor("#ef5350")
+    ) : Theme()
 }
--- app/src/main/java/yaroslavgorbach/koropapps/vocabulary/data/settings/repo/RepoSettingsImp.kt
+++ app/src/main/java/yaroslavgorbach/koropapps/vocabulary/data/settings/repo/RepoSettingsImp.kt
@@ -7,6 +7,17 @@ import yaroslavgorbach.koropapps.vocabulary.data.settings.local.model.Theme
 
 class RepoSettingsImp(private val localDataStore: SettingsDataStore) : RepoSettings {
 
+    override val themes: List<Theme>
+        get() = listOf(
+            Theme.Base(),
+            Theme.Blue(),
+            Theme.Indigo(),
+            Theme.DeepPurple(),
+            Theme.Purple(),
+            Theme.Pink(),
+            Theme.Red()
+        )
+
     override fun observeCurrentTheme(context: Context): Flow<Theme> {
         return localDataStore.getCurrentTheme(context)
     }
