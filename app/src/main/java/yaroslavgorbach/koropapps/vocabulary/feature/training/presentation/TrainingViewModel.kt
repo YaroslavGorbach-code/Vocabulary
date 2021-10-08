73cad03cc8ef876e0bcfa72ad9cefc90741c6851
YaroslavGorbach-code <yaroslavgorbach2@gmail.com>
2021-10-08 18:14:04 +0300

    Minor fixes

--- app/src/main/java/yaroslavgorbach/koropapps/vocabulary/feature/training/presentation/TrainingViewModel.kt
+++ app/src/main/java/yaroslavgorbach/koropapps/vocabulary/feature/training/presentation/TrainingViewModel.kt
@@ -1,6 +1,5 @@
 package yaroslavgorbach.koropapps.vocabulary.feature.training.presentation
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
