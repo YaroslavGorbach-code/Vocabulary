a799347e810be7b3e533c338f4807f20ca2dd66a
YaroslavGorbach-code <yaroslavgorbach2@gmail.com>
2021-11-05 09:52:38 +0200

    Improve achievements list screen UI Replace linear layout on grid

--- app/src/main/java/yaroslavgorbach/koropapps/vocabulary/feature/profile/level/ui/LevelView.kt
+++ app/src/main/java/yaroslavgorbach/koropapps/vocabulary/feature/profile/level/ui/LevelView.kt
@@ -1,12 +1,15 @@
 package yaroslavgorbach.koropapps.vocabulary.feature.profile.level.ui
 
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager.HORIZONTAL
 import yaroslavgorbach.koropapps.vocabulary.R
 import yaroslavgorbach.koropapps.vocabulary.data.achievements.local.model.Achievement
 import yaroslavgorbach.koropapps.vocabulary.databinding.FragmentLevelBinding
+import yaroslavgorbach.koropapps.vocabulary.feature.common.uikit.recycler.GridSpacingItemDecoration
 import yaroslavgorbach.koropapps.vocabulary.feature.profile.level.model.LevelInfoUi
 import yaroslavgorbach.koropapps.vocabulary.feature.profile.level.ui.recycler.AchievementsAdapter
+import yaroslavgorbach.koropapps.vocabulary.feature.profile.settings.ui.ChoseThemeDialog
 import yaroslavgorbach.koropapps.vocabulary.utils.getDrawable
 
 class LevelView(private val binding: FragmentLevelBinding, private val callback: Callback) {
@@ -16,7 +19,13 @@ class LevelView(private val binding: FragmentLevelBinding, private val callback:
         fun onAchievement(achievement: Achievement)
     }
 
+    companion object {
+        const val NUMBER_OF_GRID_SPAN = 3
+    }
+
     private var achievementsAdapter: AchievementsAdapter? = null
+
+    private var gridSpacingItemDecorator: GridSpacingItemDecoration? = null
 
     init {
         initViews()
@@ -26,9 +35,13 @@ class LevelView(private val binding: FragmentLevelBinding, private val callback:
     private fun initViews() {
         achievementsAdapter = AchievementsAdapter(callback::onAchievement)
 
+        gridSpacingItemDecorator =
+            GridSpacingItemDecoration(NUMBER_OF_GRID_SPAN, 34, true, 0)
+
         binding.achievements.listAchievements.apply {
             adapter = achievementsAdapter
-            layoutManager = LinearLayoutManager(binding.root.context, HORIZONTAL, false)
+            layoutManager = GridLayoutManager(context, NUMBER_OF_GRID_SPAN)
+            addItemDecoration(requireNotNull(gridSpacingItemDecorator))
         }
 
         binding.levels.levelImprovisationImageProgress.setImage(binding.getDrawable(R.drawable.ic_impromptu))
