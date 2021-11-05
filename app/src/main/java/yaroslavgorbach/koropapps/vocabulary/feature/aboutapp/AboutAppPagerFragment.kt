be0420cfbf5848f1d9f770e4fdaa4527fe6f238b
YaroslavGorbach-code <yaroslavgorbach2@gmail.com>
2021-11-05 15:32:10 +0200

    started creating about app pager

---
+++ app/src/main/java/yaroslavgorbach/koropapps/vocabulary/feature/aboutapp/AboutAppPagerFragment.kt
@@ -0,0 +1,9 @@
+package yaroslavgorbach.koropapps.vocabulary.feature.aboutapp
+
+import androidx.fragment.app.Fragment
+import yaroslavgorbach.koropapps.vocabulary.R
+
+class AboutAppPagerFragment: Fragment(R.layout.fragment_about_app) {
+
+
+}
