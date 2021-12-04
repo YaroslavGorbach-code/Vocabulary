51f88b443bea3b0d4e0b40bfc1bcd15e48a0f6df
Yaroslav <yaroslavgorbach2@gmail.com>
2021-12-04 22:54:25 +0200

    Implement share and rate app features

--- app/src/main/java/yaroslavgorbach/koropapps/vocabulary/feature/profile/profile/ui/ProfileFragment.kt
+++ app/src/main/java/yaroslavgorbach/koropapps/vocabulary/feature/profile/profile/ui/ProfileFragment.kt
@@ -1,6 +1,8 @@
 package yaroslavgorbach.koropapps.vocabulary.feature.profile.profile.ui
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -25,6 +27,9 @@ class ProfileFragment : Fragment(R.layout.fragment_profile), InfoDialog.Host {
     }
 
     companion object {
+        private const val APP_LINK =
+            "https://play.google.com/store/apps/details?id=yaroslavgorbach.andersen.vocabulary"
+
         fun newInstance() = ProfileFragment()
     }
 
@@ -75,11 +80,11 @@ class ProfileFragment : Fragment(R.layout.fragment_profile), InfoDialog.Host {
                 }
 
                 override fun onShare() {
-
+                    shareApp()
                 }
 
                 override fun onRate() {
-
+                    rateApp()
                 }
 
                 override fun onLevel() {
@@ -90,6 +95,27 @@ class ProfileFragment : Fragment(R.layout.fragment_profile), InfoDialog.Host {
                     showPhraseDialog(phrase)
                 }
             })
+    }
+
+    private fun shareApp() {
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, APP_LINK)
+            type = "text/plain"
+        }
+
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        startActivity(shareIntent)
+    }
+
+    private fun rateApp() {
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_VIEW
+            data = Uri.parse(APP_LINK)
+        }
+
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        startActivity(shareIntent)
     }
 
     private fun showPhraseDialog(phrase: String) {
